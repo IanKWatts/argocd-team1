cd20c2898173f9c27788400f9f4fbf68d790b211
Ian Watts <ian@48thave.com>
2024-10-17 15:29:33 -0700

    Add stress

---
+++ stress/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/load/<int:workers>')
+def load(workers):
+	os.system(f"stress --cpu {workers} --timeout 10")
+	return f"CPU load with {workers} workers started."
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80)
+
