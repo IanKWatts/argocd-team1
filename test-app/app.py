e7c4ff70c5342e930a73e5d96f8539d004ec39b5
Ian Watts <ian@48thave.com>
2024-10-18 10:32:29 -0700

    Testing

--- test-app/app.py
+++ test-app/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/load/<int:workers>')
 def load(workers):
-	os.system(f"stress --cpu {workers} --timeout 10")
-	return f"CPU load with {workers} workers started."
+	os.system("stress --cpu {workers} --timeout 10")
+	return "CPU load with {workers} workers started."
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
